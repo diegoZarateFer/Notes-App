70cf71249e62e0c4b6204387dadd84da740303ac
Diego Zárate <diegozaratefernandez300@gmail.com>
2024-04-30 13:01:38 -0600

    Fixed submodule error.

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,11 @@
+import "../styles/LoadingIndicator.css";
+
+function LoadingIndicator() {
+  return (
+    <div className="loading-container">
+      <div className="loader" />
+    </div>
+  );
+}
+
+export default LoadingIndicator;
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,59 @@
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import api from "../api";
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+import { useState, useEffect } from "react";
+
+function ProtectedRoute({ children }) {
+    const [ isAuthorized, setIsAuthorized ] = useState(null);
+
+    useEffect(() => {
+        auth().catch(() => setIsAuthorized(false));
+    }, []);
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const res = await api.post("/api/token/refresh/", {
+                refresh: refreshToken
+            });
+
+            if (res.status == 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                setIsAuthorized(true);
+            } else {
+                setIsAuthorized(false);
+            }
+        } catch (error) {
+            console.log(error);
+            setIsAuthorized(false);
+        }
+    };
+
+    const auth = async () => {
+
+        // Aun no entiendo porqua se hace esto.
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (!token) {
+            setIsAuthorized(false);
+            return;
+        }
+
+        const decoded = jwtDecode(token);
+        const tokenExpiration = decoded.exp;
+        const now = Date.now() / 1000;
+        if (tokenExpiration < now) { // Token expirado
+            await refreshToken(); // Acualizar el token.
+        } else {
+            setIsAuthorized(true); // Token no expirado.
+        }
+    };
+
+    if (isAuthorized === null) {
+        return <div>Loading ...</div>
+    }
+
+    return isAuthorized ? children : <Navigate to="/login" />
+}
+
+export default ProtectedRoute;
